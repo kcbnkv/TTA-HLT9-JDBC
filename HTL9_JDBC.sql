c387e9b6dd3fda7f3b14c18dfe16e07d4bf71f3f
Karina Cabanikova <87716339+kcbnkv@users.noreply.github.com>
2021-10-25 22:07:18 +0100

    Add files via upload

---
+++ HTL9_JDBC.sql
@@ -0,0 +1,13 @@
+/*
+-- Query: SELECT * FROM TTA.Employee
+LIMIT 0, 1000
+
+-- Date: 2021-10-25 21:47
+*/
+INSERT INTO `` (`eid`,`ename`,`email`,`mobile`,`designation`,`salary`) VALUES (1,'Marcin k','MarcinK@Yahoo.com','+44 0 987 345 123','Graphic Designer',23000);
+INSERT INTO `` (`eid`,`ename`,`email`,`mobile`,`designation`,`salary`) VALUES (2,'Patricia k','PatriciaK@yahoo.co.uk','+44 0 776 345 123','Admin Assistant',21800);
+INSERT INTO `` (`eid`,`ename`,`email`,`mobile`,`designation`,`salary`) VALUES (6,'Peter','Peter@yahoo.co.uk','+44 0 876 478 231','Intern',0);
+INSERT INTO `` (`eid`,`ename`,`email`,`mobile`,`designation`,`salary`) VALUES (12,'Ottis H','OHarris@hotmail.com','+44 0 6789 987 543','UX/UI Apprentice',19000);
+INSERT INTO `` (`eid`,`ename`,`email`,`mobile`,`designation`,`salary`) VALUES (23,'Piotr P','PPiotrc@outlook.co.uk','+421 0 389 004 231','Contractor',30000);
+INSERT INTO `` (`eid`,`ename`,`email`,`mobile`,`designation`,`salary`) VALUES (345,'Pete Shush','PeteShush@yahoo.co.uk','+44 0 987 345 678','Software Developer',28500);
+INSERT INTO `` (`eid`,`ename`,`email`,`mobile`,`designation`,`salary`) VALUES (768,'karolina','Karolina@yahoo.co.uk','+421 0 339 004 231','Contractor',42350);
